8533bf973405057e891c3c05dd732a01a6383927
PiiosS <popov01artem@mail.ru>
2024-06-25 09:19:02 +0400

    Removed the old code from map.py Added the procedure for updating the schedule in the database The order of commands for the bot has been changed. Now the bot will always respond to messages if it has a command to do so Added the ability to log messages to the database and to a separate file too Fixed a large message error

--- py_s/bot.py
+++ py_s/bot.py
@@ -2,46 +2,86 @@ import telebot
 import pymysql
 from datetime import date, timedelta
 import data
+import map
+import csv
+
 
 bot = telebot.TeleBot(data.TOKEN)
-db_connection = pymysql.connect(host=data.HOST, user=data.USER, password=data.PASSWORD, database=data.DATABASE)
-db_cursor = db_connection.cursor()
-message=''
+connection = pymysql.connect(host=data.HOST, user=data.USER, password=data.PASSWORD, database=data.DATABASE)
+cursor = connection.cursor()
+message = ''
+message_text = ''
+
+
+def log(user_id, first_name, user_name, user_message, bot_response):
+    insert_query = f"""INSERT INTO log (time_m, user_id, first_name, user_name, user_message, bot_response)
+     VALUES (NOW(),'{user_id}','{first_name}','{user_name}','{user_message}','{bot_response}')"""
+    try:
+        cursor.execute(insert_query)
+        connection.commit()
+    except: bot_response="Ошибка подключения к БД для логирования "+bot_response
+    file_path = 'id.csv'
+    with open(file_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([user_id, first_name, user_name, user_message, bot_response])
+        print(user_id, first_name, user_name, user_message, bot_response)
+
+
+@bot.message_handler(commands=['update'])
+def start_message(message):
+    try:
+        map.schedule()
+        message_text='Обновлено'
+    except:
+        message_text='Ошибка обновления'
+    bot.send_message(message.chat.id, message_text)
+    log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Привет, чем я могу тебе помочь?")
-    bot.send_message(message.chat.id, "Для вывода данных на текущий день напиши /today, для вывода данных на следующий день напиши /next_day")
+    message_text='Для вывода данных на текущий день напиши /today, для вывода данных на следующий день напиши /next_day'
+    bot.send_message(message.chat.id, message_text)
+    log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
-    bot.send_message(message.chat.id, "Для вывода данных на текущий день напиши /today, для вывода данных на следующий день напиши /next_day")
+    message_text="Для вывода данных на текущий день напиши /today, для вывода данных на следующий день напиши /next_day"
+    bot.send_message(message.chat.id,message_text)
+    log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
+
 
 @bot.message_handler(commands=['today'])
 def get_today_data(message):
     current_date = date.today()
-    print(message.chat.id,"Запросил",current_date)
+    print(message.chat.id, "Запросил", current_date)
     try:
-        db_cursor.execute("SELECT date, pair, signature, classroom, classroom_building, subject FROM schedule WHERE date = %s", (current_date,))
-        rows = db_cursor.fetchall()
+        cursor.execute(
+            "SELECT date, pair, signature, classroom, classroom_building, subject FROM schedule WHERE date = %s LIMIT 5",
+            current_date)
+        rows = cursor.fetchall()
         if rows:
             message_text = "Данные на текущий день:\n"
             for row in rows:
-                date_str, pair, signature, classroom, classroom_building, subject = row
-                message_text += f"Дата: {date_str}, Время занятия: {pair}, Преподаватель: {signature}, Предмет: {subject}, Кабинет: {classroom}, Учебный корпус: {classroom_building}\n"
+                date_s, pair, signature, classroom, classroom_building, subject = row
+                message_text += f"Дата: {date_s}, Время занятия: {pair}, Преподаватель: {signature}, Предмет: {subject}, Кабинет: {classroom}, Учебный корпус: {classroom_building}\n"
         else:
             message_text = "На текущий день данных нет"
     except:
-        message_text = "Произошла ошибка при получении данных из базы данных"
+        message_text = "На текущий день данных нет"
     bot.send_message(message.chat.id, message_text)
+    log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
+
 
 @bot.message_handler(commands=['next_day'])
 def get_next_day_data(message):
     next_date = date.today() + timedelta(days=1)
-    print(message.chat.id,"Запросил",next_date)
+    print(message.chat.id, "Запросил", next_date)
     try:
-        db_cursor.execute("SELECT date, pair, signature, classroom, classroom_building, subject FROM schedule WHERE date = %s", (next_date,))
-        rows = db_cursor.fetchall()
+        cursor.execute(
+            "SELECT date, pair, signature, classroom, classroom_building, subject FROM schedule WHERE date = %s LIMIT 5",
+            (next_date,))
+        rows = cursor.fetchall()
         if rows:
             message_text = "Данные на следующий день:\n"
             for row in rows:
@@ -52,6 +92,9 @@ def get_next_day_data(message):
     except:
         message_text = "На следующий день данных нет"
     bot.send_message(message.chat.id, message_text)
+    log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
+
+
 @bot.message_handler(commands=['Ксюша'])
 def ksenia_msg(message):
     print('Отправляю 962847585')
@@ -59,11 +102,20 @@ def ksenia_msg(message):
     img = open('cat.jpg', 'rb')
     bot.send_photo(5169161016, img)
     bot.send_message(message.chat.id, message_text)
+    log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
+
 
 @bot.message_handler(commands=['стоп'])
 def stop_msg(message):
     print('Протокол "пока"')
-    message_text = "Ня, пока ❤️"
+    message_text = "Ня, пока"
     bot.send_message(message.chat.id, message_text)
+    log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
     bot.stop_bot()
+
+@bot.message_handler()
+def all_message(message):
+    log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
+
 bot.polling()
+
--- py_s/map.py
+++ py_s/map.py
@@ -1,4 +1,4 @@
-import data.py
+import data
 import pymysql
 import requests
 
@@ -6,46 +6,37 @@ connection = pymysql.connect(host=data.HOST, user=data.USER, password=data.PASSW
 cursor = connection.cursor()
 
 
+def schedule():
+    url = "https://api.xn--80aai1dk.xn--p1ai/api/schedule?range=1"  #&subdivision_cod=1&group_name=5134"
 
-url = "https://api.xn--80aai1dk.xn--p1ai/api/schedule?range=1&subdivision_cod=1&group_name=5134"
+    PAIR_TIMES = {
+        '1': '08:30-10:00',
+        '2': '10:15-11:45',
+        '3': '12:20-13:50',
+        '4': '14:00-15:30',
+        '5': '15:45-17:15',
+        '6': '17:30-19:00',
+        '7': '19:10-20:40',
+    }
+    cursor.execute("""TRUNCATE TABLE schedule""")
 
-PAIR_TIMES = {
-    '1': '08:30-10:00',
-    '2': '10:15-11:45',
-    '3': '12:20-13:50',
-    '4': '14:00-15:30',
-    '5': '15:45-17:15',
-    '6': '17:30-19:00',
-    '7': '19:10-20:40',
-}
-cursor.execute("""TRUNCATE TABLE schedule""")
-with open('schedule_data.csv', mode='w', newline='') as csvfile:
-    #fieldnames = ['date', 'pair', 'subject', 'signature', 'classroom', 'classroom_building', 'group_name']
-    #writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
-    #writer.writeheader()
     response = requests.get(url)
     data = response.json()
     print(f': {response.status_code} ({len(data)})')
     for schedule in data:
-        DATEarr=schedule['date'].split('.')
-        DATE=str(DATEarr[2]+DATEarr[1]+DATEarr[0])
-        #writer.writerow({
-        #    'date': schedule['date'].strip(),
-        #    'pair': PAIR_TIMES[schedule['pair'].strip()],
-        #    'signature': schedule['signature'].strip(),
-        #    'classroom': CLASS.strip(),
-        #    'classroom_building': schedule['classroom_building'].strip(),
-        #    'group_name': schedule['group_name'].strip(),
-        #})
-
+        DATEarr = schedule['date'].split('.')
+        DATE = str(DATEarr[2] + DATEarr[1] + DATEarr[0])
         insert_query = """
-        INSERT INTO schedule (date, pair, subject, signature, classroom, classroom_building, group_name) 
-        VALUES (%s, %s, %s, %s, %s, %s, %s)
-        """
+                INSERT INTO schedule (date, pair, subject, signature, classroom, classroom_building, group_name) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                """
         cursor.execute(insert_query, (DATE.strip(), PAIR_TIMES[schedule['pair'].strip()], schedule['subject'].strip(),
                                       schedule['signature'].strip(), schedule['classroom'].strip(),
                                       schedule['classroom_building'].strip(), schedule['group_name'].strip()))
 
-# Подтверждение изменений в базе данных и закрытие соединения
-connection.commit()
-connection.close()
+    # Подтверждение изменений в базе данных и закрытие соединения utf8_unicode_ci
+
+    connection.commit()
+    connection.close()
+
+#schedule()
