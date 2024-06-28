673d604f52e58731264e3d9124a230aab19b004f
PiiosS <popov01artem@mail.ru>
2024-06-28 10:47:34 +0400

    Add connection, schedule on date procedures Now the bot can reconnect to the database

--- py_s/bot.py
+++ py_s/bot.py
@@ -1,34 +1,60 @@
 import telebot
 import pymysql
-from datetime import date, timedelta
-import data
+from datetime import datetime, date, timedelta
+import sdata
 import map
 import csv
 
 
-bot = telebot.TeleBot(data.TOKEN)
-connection = pymysql.connect(host=data.HOST, user=data.USER, password=data.PASSWORD, database=data.DATABASE)
+bot = telebot.TeleBot(sdata.TOKEN)
+connection = pymysql.connect(host=sdata.HOST, user=sdata.USER, password=sdata.PASSWORD, database=sdata.DATABASE)
 cursor = connection.cursor()
 message = ''
 message_text = ''
 
-
+def conn(query):
+    connection = pymysql.connect(host=sdata.HOST, user=sdata.USER, password=sdata.PASSWORD, database=sdata.DATABASE)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+#Решить проблему закрывания (разрыва) соединения
 def log(user_id, first_name, user_name, user_message, bot_response):
-    insert_query = f"""INSERT INTO log (time_m, user_id, first_name, user_name, user_message, bot_response)
+    insert_query = f"""INSERT INTO log_m (time_m, user_id, first_name, user_name, user_message, bot_response)
      VALUES (NOW(),'{user_id}','{first_name}','{user_name}','{user_message}','{bot_response}')"""
+    map.del_sign(insert_query)
     try:
-        cursor.execute(insert_query)
-        connection.commit()
+        conn(insert_query)
     except: bot_response="Ошибка подключения к БД для логирования "+bot_response
     file_path = 'id.csv'
     with open(file_path, mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([user_id, first_name, user_name, user_message, bot_response])
         print(user_id, first_name, user_name, user_message, bot_response)
-
+def schedule_date(s_date, message):
+    connection = pymysql.connect(host=sdata.HOST, user=sdata.USER, password=sdata.PASSWORD, database=sdata.DATABASE)
+    cursor = connection.cursor()
+    query=f"SELECT `sdate`, `pair`, `subject`, `signature`, `classroom`, `classroom_building`, `group_name` FROM `schedule` WHERE `sdate` = '{s_date}' LIMIT 5"
+    cursor.execute(query)
+    try:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        print(rows)
+        if rows:
+            message_text = "Данные на текущий день:\n"
+            for row in rows:
+                sdate, pair, subject, signature, classroom, classroom_building, group_name = row
+                message_text += f"Дата: {sdate}, Время занятия: {pair}, Преподаватель: {signature}, Предмет: {subject}, Кабинет: {classroom}, Учебный корпус: {classroom_building}\n\n"
+        else:
+            message_text = f"На {s_date} данных нет"
+    except:
+        message_text = f"На {s_date} данных нет"
+    bot.send_message(message.chat.id, message_text)
+    log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
 
 @bot.message_handler(commands=['update'])
 def start_message(message):
+    bot.send_message(message.chat.id,'Обновляю')
     try:
         map.schedule()
         message_text='Обновлено'
@@ -49,50 +75,20 @@ def help_message(message):
     message_text="Для вывода данных на текущий день напиши /today, для вывода данных на следующий день напиши /next_day"
     bot.send_message(message.chat.id,message_text)
     log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
-
+    #print('Гит не гитится, идея не идеется')
 
 @bot.message_handler(commands=['today'])
 def get_today_data(message):
     current_date = date.today()
     print(message.chat.id, "Запросил", current_date)
-    try:
-        cursor.execute(
-            "SELECT date, pair, signature, classroom, classroom_building, subject FROM schedule WHERE date = %s LIMIT 5",
-            current_date)
-        rows = cursor.fetchall()
-        if rows:
-            message_text = "Данные на текущий день:\n"
-            for row in rows:
-                date_s, pair, signature, classroom, classroom_building, subject = row
-                message_text += f"Дата: {date_s}, Время занятия: {pair}, Преподаватель: {signature}, Предмет: {subject}, Кабинет: {classroom}, Учебный корпус: {classroom_building}\n"
-        else:
-            message_text = "На текущий день данных нет"
-    except:
-        message_text = "На текущий день данных нет"
-    bot.send_message(message.chat.id, message_text)
-    log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
+    schedule_date(current_date, message)
 
 
 @bot.message_handler(commands=['next_day'])
 def get_next_day_data(message):
     next_date = date.today() + timedelta(days=1)
     print(message.chat.id, "Запросил", next_date)
-    try:
-        cursor.execute(
-            "SELECT date, pair, signature, classroom, classroom_building, subject FROM schedule WHERE date = %s LIMIT 5",
-            (next_date,))
-        rows = cursor.fetchall()
-        if rows:
-            message_text = "Данные на следующий день:\n"
-            for row in rows:
-                date_str, pair, signature, classroom, classroom_building, subject = row
-                message_text += f"Дата: {date_str}, Время занятия: {pair}, Преподаватель: {signature}, Предмет: {subject}, Кабинет: {classroom}, Учебный корпус: {classroom_building}\n"
-        else:
-            message_text = "На следующий день данных нет"
-    except:
-        message_text = "На следующий день данных нет"
-    bot.send_message(message.chat.id, message_text)
-    log(message.chat.id, message.from_user.first_name, message.from_user.username, message.text, message_text)
+    schedule_date(next_date, message)
 
 
 @bot.message_handler(commands=['Ксюша'])
--- py_s/map.py
+++ py_s/map.py
@@ -1,11 +1,13 @@
-import data
+import sdata
 import pymysql
 import requests
-
-connection = pymysql.connect(host=data.HOST, user=data.USER, password=data.PASSWORD, database=data.DATABASE)
-cursor = connection.cursor()
+import re
 
 
+def del_sign(text):
+    return re.sub(r'[!@#$%^&*\";:?]', '', text)
+def conn():
+    return pymysql.connect(host=sdata.HOST, user=sdata.USER, password=sdata.PASSWORD, database=sdata.DATABASE)
 def schedule():
     url = "https://api.xn--80aai1dk.xn--p1ai/api/schedule?range=1"  #&subdivision_cod=1&group_name=5134"
 
@@ -18,25 +20,36 @@ def schedule():
         '6': '17:30-19:00',
         '7': '19:10-20:40',
     }
+
+    connection = conn()
+    cursor = connection.cursor()
     cursor.execute("""TRUNCATE TABLE schedule""")
+
 
     response = requests.get(url)
     data = response.json()
     print(f': {response.status_code} ({len(data)})')
     for schedule in data:
         DATEarr = schedule['date'].split('.')
-        DATE = str(DATEarr[2] + DATEarr[1] + DATEarr[0])
-        insert_query = """
-                INSERT INTO schedule (date, pair, subject, signature, classroom, classroom_building, group_name) 
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
+        DATE = str(DATEarr[2] + '-' + DATEarr[1] + '-' + DATEarr[0])
+        pair=PAIR_TIMES[schedule['pair'].strip()]
+        subject = del_sign(schedule['subject'].strip())
+        if subject=='':
+            subject = del_sign(schedule['prim'].strip())
+        signature = del_sign(schedule['signature'].strip())
+        classroom = del_sign(schedule['classroom'].strip())
+        classroom_building = del_sign(schedule['classroom_building'].strip())
+        group_name = del_sign(schedule['group_name'].strip())
+
+        insert_query = f"""
+                INSERT INTO `schedule`(`sdate`, `pair`, `subject`, `signature`, `classroom`, `classroom_building`, `group_name`) VALUES ("{DATE}","{pair}","{subject}","{signature}","{classroom}","{classroom_building}","{group_name}")
                 """
-        cursor.execute(insert_query, (DATE.strip(), PAIR_TIMES[schedule['pair'].strip()], schedule['subject'].strip(),
-                                      schedule['signature'].strip(), schedule['classroom'].strip(),
-                                      schedule['classroom_building'].strip(), schedule['group_name'].strip()))
+        connection = conn()
+        cursor = connection.cursor()
+        cursor.execute(insert_query)
+# Подтверждение изменений в базе данных и закрытие соединения utf8_unicode_ci
 
-    # Подтверждение изменений в базе данных и закрытие соединения utf8_unicode_ci
-
-    connection.commit()
+        connection.commit()
     connection.close()
-
+x=1+1
 #schedule()
